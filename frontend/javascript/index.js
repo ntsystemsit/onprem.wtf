c045f947d8b8f4a8df5d79052eb6ba09c62a9bb1
tomtorggler <tto@expertsinside.com>
2022-09-10 16:54:24 +0200

    remove service worker

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -14,9 +14,20 @@ import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
 console.info("onprem.wtf is loaded!")
 
-if ('serviceWorker' in navigator && !(/localhost/.test(window.location.href))) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/serviceworker.js')
-        console.log('Servie worker registered.');
-    });
-}
+navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    if (!registrations.length) {
+      console.log('No serviceWorker registrations found.')
+      return
+    }
+    for(let registration of registrations) {
+      registration.unregister().then(function (boolean) {
+        console.log(
+          (boolean ? 'Successfully unregistered' : 'Failed to unregister'), 'ServiceWorkerRegistration\n' +
+          (registration.installing ? '  .installing.scriptURL = ' + registration.installing.scriptURL + '\n' : '') +
+          (registration.waiting ? '  .waiting.scriptURL = ' + registration.waiting.scriptURL + '\n' : '') +
+          (registration.active ? '  .active.scriptURL = ' + registration.active.scriptURL + '\n' : '') +
+          '  .scope: ' + registration.scope + '\n'
+        )
+      })
+    }
+  })
